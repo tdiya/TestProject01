ca1f0a4c0c9e19bfc3ff1832cf4921875442f3a4
Tahmina Sultana <Tahmina Sultana@192.168.0.12>
2019-10-08 20:53:57 -0400

    created a class TestJava.java

---
+++ githuh/src/githuh1/TestJava.java
@@ -0,0 +1,14 @@
+package githuh1;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		System.out.println(a);
+		
+
+	}
+
+}
